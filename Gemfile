532cf9a6d6d2515fedc3e786347adef53e4f6867
Evan Light <evan@tripledogdare.net>
2010-11-03 21:14:20 -0400

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,16 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.1'
+
+gem 'pg'
+
+gem 'formtastic'
+gem 'inherited_resources', '1.1.2'
+gem 'active_merchant'
+
+# Bundle gems for the local environment. Make sure to
+# put test-only gems in this group so their generators
+# and rake tasks are available in development mode:
+# group :development, :test do
+#   gem 'webrat'
+# end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RemoteJobs::Application.config.secret_token = 'a5314ad9f38c3da9f3db2c8c8e9ceecb4e1f31b3d3b64e2b56f0444a67bc7432f108e8743216503dfd154a12c9b7a46215e1b060abce52ca84312bc98d8f87e9'
