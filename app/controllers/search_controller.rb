f11579bf11c77bff21a6350a29eb66ba579681c9
David Browning <djbrowning@gmail.com>
2010-11-12 11:55:18 -0500

    Added AREL searching to prevent from having to use Solr for now

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,15 +3,41 @@ class SearchController < ApplicationController
   def filter
     begin
       #store_filters_in_session
-      render(:partial => "job_postings/job_posting", :collection => filtered_job_postings.results)
+      render(:partial => "job_postings/job_posting", :collection => filtered_job_postings)
     rescue Exception => e
       render :text => e
     end
   end
 
   private
-
+  
   def filtered_job_postings
+    job_postings = JobPosting.where(:enabled => true).order("created_at DESC")
+    
+    categories = []
+    ["design", "development", "copywriting", "management"].each do |category|
+      categories << "'#{category.capitalize}'" if params[category.to_sym].present?
+    end
+    job_postings = job_postings.where("category IN (#{categories.join(",")})") unless categories.empty?
+    
+    job_types = []
+    ["freelancer", "employee"].each do |job_type|
+      job_types << "'#{job_type.capitalize}'" if params[job_type.to_sym].present?
+    end
+    job_postings = job_postings.where("job_type IN (#{job_types.join(",")})") unless job_types.empty?
+    
+    payment_types = []
+    ["hourly", "salary"].each do |payment_type|
+      payment_types << "'#{payment_type.capitalize}'" if params[payment_type.to_sym].present?
+    end
+    job_postings = job_postings.where("payment_type IN (#{payment_types.join(",")})") unless payment_types.empty?
+    
+    job_postings = job_postings.where("UPPER(title) LIKE ?", "%#{params[:search].upcase}%") unless params[:search].blank?
+
+    return job_postings
+  end
+
+  def filtered_job_postings_with_solr
     Sunspot.search(JobPosting) do
       fulltext params[:search] if params[:search].present?
       
