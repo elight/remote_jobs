5541f55498eaf565200c4980622d285ee6305acb
David Browning <djbrowning@gmail.com>
2010-11-23 00:13:45 -0500

    Added placeholder to job term in form. Updated some styles to handle new input types. Changed RAILS_ROOT to Rails.root in environment config

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,7 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
-heroku_env = File.join(RAILS_ROOT, 'config', 'heroku_env.rb')
+heroku_env = File.join(Rails.root, 'config', 'heroku_env.rb')
 load(heroku_env) if File.exists?(heroku_env)
 
 # Initialize the rails application
