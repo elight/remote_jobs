2668f1493193c8e5bf693f7b5e12ec8cabf25918
Evan Light <evan@tripledogdare.net>
2010-12-01 14:38:17 -0500

    Modified to pull ActiveMerchant mode from ENV

--- config/environment.rb
+++ config/environment.rb
@@ -4,5 +4,9 @@ require File.expand_path('../application', __FILE__)
 heroku_env = File.join(Rails.root, 'config', 'heroku_env.rb')
 load(heroku_env) if File.exists?(heroku_env)
 
+if ENV["ACTIVE_MERCHANT_MODE"]
+  ActiveMerchant::Billing::Base.mode = ENV["ACTIVE_MERCHANT_MODE"].to_sym
+end
+
 # Initialize the rails application
 RemoteJobs::Application.initialize!
