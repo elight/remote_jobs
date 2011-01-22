68b42b159d969e35c264771d3c61e36fb790d33e
Evan Light <evan@tripledogdare.net>
2011-01-22 17:05:39 -0500

    mend

---
+++ test/factories.rb
@@ -0,0 +1,25 @@
+Factory.define :job_posting do |j|
+  j.sequence(:title)      { |n| "Uncle Fucker #{n}" }
+  j.description           "Duh"
+  j.job_type              "Employee"
+  j.payment_type          "Salary"
+  j.how_to_apply          "Fuck someone's uncle"
+  j.hiring_criteria       "Experience fucking uncles"
+  j.category              "Management"
+  j.company_name          "Uncle Fuckers R Us"
+  j.email_address         "somedumbfucker@unclefuckersr.us"
+  j.first_name            "Some Dumb"
+  j.last_name             "Fucker"
+  j.street_address1       "1 Dumb Fuck Way"
+  j.city                  "Fuck You"
+  j.state                 "DE"
+  j.country               "United States"
+  j.phone_number          "555-555-5555"
+end
+
+Factory.define :credit_card do |c|
+  c.number                "5105105105105100"
+  c.month                 "12"
+  c.year                  { (Date.today.year + 10).to_s }
+  c.cvv                   "123"
+end
