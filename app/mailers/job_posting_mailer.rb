f5428cc3f7dd8b19c78103d0bfbdb25d3d22533e
Evan Light <evan@tripledogdare.net>
2011-01-27 10:45:51 -0500

    Duh don't use hard-coded URL to remote.jobs

--- app/mailers/job_posting_mailer.rb
+++ app/mailers/job_posting_mailer.rb
@@ -3,15 +3,15 @@ class JobPostingMailer < ActionMailer::Base
 
   def new_job_posting_email(job_posting)
     @title = job_posting.title
-    @url = "http://remote.jobs/edit/#{job_posting.uid}"
+    @url = "http://remote-jobs.com/edit/#{job_posting.uid}"
     mail :to => job_posting.email_address,
          :subject => "[Remote Jobs] Your job has been created"
   end
   
   def job_posting_receipt(job_posting)
     @title = job_posting.title
-    @show_url = "http://remote.jobs/jobs/#{job_posting.to_param}"
-    @edit_url = "http://remote.jobs/edit/#{job_posting.uid}"
+    @show_url = "http://remote-jobs.com/jobs/#{job_posting.to_param}"
+    @edit_url = "http://remote-jobs.com/edit/#{job_posting.uid}"
     @did_use_coupon = job_posting.coupon.present?
     mail :to => job_posting.email_address,
          :subject => "[Remote Jobs] Your receipt"
