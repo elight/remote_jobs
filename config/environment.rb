c629cec3b091f5876887b189d20ee73a50afe9a6
Evan Light <evan@tripledogdare.net>
2011-02-20 15:24:02 -0500

    Only load heroku_env for non-production

--- config/environment.rb
+++ config/environment.rb
@@ -1,8 +1,10 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
-heroku_env = File.join(Rails.root, 'config', 'heroku_env.rb')
-load(heroku_env) if File.exists?(heroku_env)
+unless Rails.env == 'production'
+  heroku_env = File.join(Rails.root, 'config', 'heroku_env.rb')
+  load(heroku_env) if File.exists?(heroku_env)
+end
 
 if ENV["ACTIVE_MERCHANT_MODE"]
   ActiveMerchant::Billing::Base.mode = ENV["ACTIVE_MERCHANT_MODE"].to_sym
